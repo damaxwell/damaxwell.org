039038d85d0d87937ead2506e7bc0a906c4c6bf7
David Maxwell <damaxwell@alaska.edu>
2026-05-17 16:15:57 -0800

    Update kana-match app
    Source: 312ca68 attempt to fix redirect errors on iOS PWA

--- kana-match/sw.js
+++ kana-match/sw.js
@@ -1,20 +1,38 @@
-const CACHE = 'kana-v8';
-const ASSETS = ['./index.html', './style.css', './game.js', './manifest.json', './icon-192.png', './icon-512.png', './gear.png'];
+const CACHE = "kana-v8";
+const ASSETS = [
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./gear.png",
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      caches.match("./index.html").then((r) => r || fetch(e.request)),
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
